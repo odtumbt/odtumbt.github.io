d5975859568750921dc00f530812343a57e9a92b
berkaycagli <35698781+berkaycagli@users.noreply.github.com>
2020-07-20 01:01:37 +0300

    Copyright mobile

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"main.dart.js": "774cbc0be6c5a25ae832967f7beee901"
+"main.dart.js": "5b22b89509ccbd42882c79145d99d044"
 };
 
 // The application shell files that are downloaded before a service worker can
